905f16078156c0c91b0706a5c9bf46342c6d5269
MondoBiscuit <bradleofabian@gmail.com>
2018-08-11 20:18:26 -0400

    html finished, css done, working on logic

---
+++ app/data/friend.js
@@ -0,0 +1,52 @@
+var friendArray = [
+    {
+    name:"Tom Hanks",
+    photo:"http://maggiesemple.com/wp-content/uploads/2016/11/tomyoung.jpg",
+    scores:[
+        5,
+        1,
+        5,
+        5,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    name:"Arnold Schwarzenegger ",
+    photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Arnold_Schwarzenegger_%281974%29.jpg/220px-Arnold_Schwarzenegger_%281974%29.jpg",
+    scores:[
+        5,
+        3,
+        4,
+        3,
+        5,
+        3,
+        3,
+        5,
+        4,
+        3
+      ]
+  },
+  {
+    name:"Bruce Willis",
+    photo:"http://images6.fanpop.com/image/photos/32100000/Bruce-Willis-3-bruce-willis-32178000-315-433.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+];
+
+module.exports = friendArray;
