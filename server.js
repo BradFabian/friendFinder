4274442b80dff873ceff6c2cc5b3f23387747bbf
MondoBiscuit <bradleofabian@gmail.com>
2018-08-12 22:28:25 -0400

    roadmap update

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json());
 
 //roadMap to routes
 
-require("/app/routing/apiroutes")(app);
-require("/app/routing/htmlroutes")(app);
+require("./app/routing/apiroutes")(app);
+require("./app/routing/htmlroutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
